8ca9e8ded98f6d4adcea6d53881d66aff9c6dec1
unknown <achyutpaudel24@gmail.com>
2021-06-21 10:50:21 +0545

    hello

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { registeruser } from "../redux/actions/actions";
 export default function Register() {
   const [email, setemail] = useState("");
